8bdad11394eb19ab1f7b3cbfaff0acd7f10cc6ac
Valentin Minder <valentinminder@users.noreply.github.com>
2016-11-23 18:48:17 +0100

    test different font sizes

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 # must be imported first to prevent issues
 from kivy.config import Config
-Config.set('graphics', 'width', 1366)
-Config.set('graphics', 'height', 768)
+Config.set('graphics', 'fullscreen', 0) #set to 'auto' for production
+Config.set('graphics', 'width', 1366) #1366
+Config.set('graphics', 'height', 768) #768
+
+
+Config.set('graphics', 'position', 'custom') # 'auto'
+Config.set('graphics', 'top', 0)
+Config.set('graphics', 'left', 0)
+
+Config.set('graphics', 'show_cursor', 0)
+Config.set('graphics', 'borderless', 0)
+Config.set('graphics', 'resizable', 1)
 
 from kivy.app import App
 from kivy.uix.button import Button
 
 from kivy.core.audio import SoundLoader
+from kivy.properties import ObjectProperty, StringProperty
 from kivy.lang import Builder
+from kivy.factory import Factory
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.label import Label
+from kivy.properties import ListProperty
 
 # loading widget instructions
 Builder.load_file('screens.kv')
@@ -20,14 +34,101 @@ MANAGER = None
 # pre-load game music
 music = SoundLoader.load("audio/garden_base.wav")
 
+# Creating a backgroundable label (by default a blank background)
+#http://robertour.com/2015/07/15/kivy-label-or-widget-with-background-color-property/
+Builder.load_string("""
+<LabelB>:
+  background_color: 1,1,1,1
+  canvas.before:
+    Color:
+      rgba: self.background_color
+    Rectangle:
+      pos: self.pos
+      size: self.size
+""")
+
+
+class LabelB(Label):
+    background_color = ListProperty([1,1,1,1])
+
+Factory.register('KivyB', module='LabelB')
+
+#thanks  @Mathieu Virbel
+# https://andnovar.wordpress.com/2011/08/03/kivy-label-with-background/
+class LabelC(Label):
+    pass
+
 # specialized version of screen that handles keyboard presses and has a cursor system,
 # used for all the screens outside the loading screen
 class KeyScreen(Screen):
     pass
 
+class BackKeyScreen(KeyScreen):
+    def __init__(self, previous, **kwargs):
+        super(BackKeyScreen, self).__init__(**kwargs)
+
+        # set previous screen for back button
+        self.previous = previous
+
+    # returns to previous screen
+    def back(self):
+        self.manager.switch_to(self.previous, direction='right')
+
+class LoadingScreen(Screen):
+    pass
+
+
 # screen corresponding to the main menu
 class MainMenuScreen(KeyScreen):
-    pass
+    # layout containing the screen's buttons, used for cursor function
+    layout = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super(MainMenuScreen, self).__init__(**kwargs)
+
+        # cursor options
+        self.cursor_array = self.layout.children[:-1]
+        self.cursor_reverse = True
+        self.cursor_wrap = True
+
+    # switches to the how to screen
+    def test(self):
+        self.manager.switch_to(TestScreen(name="Test", previous=self), direction='left')
+
+    # switches to the training mode menu
+    def game(self):
+        self.manager.switch_to(GameScreen(name="Game", previous=self), direction='left')
+
+# "test" screen
+class TestScreen(BackKeyScreen):
+    # reference to back button for cursor use
+    bback = ObjectProperty()
+    # text contained on the screen, stored here for ease of reading
+    text = StringProperty()
+
+    def __init__(self, **kwargs):
+        super(TestScreen, self).__init__(**kwargs)
+
+        # cursor can only select back button
+        self.cursor_array = [self.bback]
+        self.cursor_wrap = True
+
+        # main text for the screen
+        self.text = "[i]italic [b]and[/i] bold[/b] black [b][color=ffffff]white[/color] [color=ff3333]red[/color][/b]"
+
+
+# screen for picking training mode difficulty
+class GameScreen(BackKeyScreen):
+    # layout containing the screen's buttons, used for cursor function
+    layout = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super(GameScreen, self).__init__(**kwargs)
+
+        # cursor options
+        self.cursor_array = self.layout.children[:-1]
+        self.cursor_reverse = True
+        self.cursor_wrap = True
 
 # main application class
 class TouchGardenApp(App):
@@ -51,7 +152,6 @@ class TouchGardenApp(App):
         # set starting screen
         self.manager.switch_to(MainMenuScreen(name="MainMenu"))
         return self.manager
-        #return Button(text='Hello World')
 
 # launch the app
 if __name__ == '__main__':
